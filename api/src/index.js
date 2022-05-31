e1000b9fe85e4789407e41c20d1adbd1cf648eb1
LucaasAndrade <lucasmarcelodeandradesilva@gmail.com>
2022-05-31 15:08:08 -0300

    Estudo de End Points - OK

--- api/src/index.js
+++ api/src/index.js
@@ -1,1 +1,11 @@
 import 'dotenv/config'
+
+import express from 'express';
+import cors from 'cors';
+import { con } from './repository/connection.js'
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+server.listen(process.env.PORT, () => console.log(`Conectado na porta ${process.env.PORT}`));
